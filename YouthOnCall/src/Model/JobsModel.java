67f235ed5434baf3550af6d07d5099a3ddd15c31
co075oh <co075oh@W5136980.ena.us.experian.local>
2016-05-27 16:52:37 -0500

    Merge origin/master

--- YouthOnCall/src/Model/JobsModel.java
+++ YouthOnCall/src/Model/JobsModel.java
@@ -10,5 +10,70 @@ package Model;
  * @author co075oh
  */
 public class JobsModel {
-    
+    private int id;
+    private String name;
+    private String description;
+    private float pay;
+    private int memberID;
+    private int youthID;
+    private String status;
+
+    public JobsModel() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getPay() {
+        return pay;
+    }
+
+    public void setPay(float pay) {
+        this.pay = pay;
+    }
+
+    public int getMemberID() {
+        return memberID;
+    }
+
+    public void setMemberID(int memberID) {
+        this.memberID = memberID;
+    }
+
+    public int getYouthID() {
+        return youthID;
+    }
+
+    public void setYouthID(int youthID) {
+        this.youthID = youthID;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
